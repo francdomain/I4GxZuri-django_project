5fa090a6c2d954c0830cfd44ff837c74e2f5e22e
Francis Chukwuoma <cfrancmail@gmail.com>
2022-10-20 23:20:05 +0100

    my music app

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=250)
+    likes = models.IntegerField()
+    date_released = models.DateTimeField(auto_now=True)
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Lyric(models.Model):
+    content = models.TextField()
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
--- songcrud/musicapp/views.py
+++ songcrud/musicapp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    return HttpResponse("<h1>This is the index page</h1>")
